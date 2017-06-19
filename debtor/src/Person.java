754c2effbe49731c87c5ab9686dfb6c8128743f8
Alex Yang <alexyang@Alexs-MacBook-Pro-4.local>
2017-06-19 00:01:47 -0700

    documentation for Person.java

--- debtor/src/Person.java
+++ debtor/src/Person.java
@@ -6,6 +6,10 @@ public class Person {
     private double debt;
     private double credit;
 
+    /**
+     * Creates a new person with a name.
+     * @param name the name of the person
+     */
     public Person(String name) {
         this.name = name;
         debt = 0;
@@ -28,10 +32,18 @@ public class Person {
         return credit - debt;
     }
 
+    /**
+     * adds amount debt
+     * @param amount the amount of debt to be added
+     */
     public void addDebt(double amount) {
         debt += amount;
     }
 
+    /**
+     * adds amount credit
+     * @param amount the amount of credit to be added
+     */
     public void addCredit(double amount) {
         credit += amount;
     }
